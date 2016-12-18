169cb9556a1aba5d8533fcdf31adf68c8b006e0e
Nino Ross Rodriguez <oninross@gmail.com>
2016-12-18 14:22:33 +1100

    - updated favicon - updated theme - index is the new search

--- src/_assets/btt/js/_busStop.js
+++ src/_assets/btt/js/_busStop.js
@@ -7,71 +7,90 @@ let loader = '<div class="loader"><svg class="circular" viewBox="25 25 50 50"><c
 
 $(() => {
     if ($('.timetable').length) {
-        let $xml = '';
-
-        $xml = '<?xml version="1.0" encoding="iso-8859-1" standalone="yes"?>';
-        $xml += '<Siri version="2.0" xmlns:ns2="http://www.ifopt.org.uk/acsb" xmlns="http://www.siri.org.uk/siri" xmlns:ns4="http://datex2.eu/schema/2_0RC1/2_0" xmlns:ns3="http://www.ifopt.org.uk/ifopt">';
-
-
-        // Check status
-        //$xml += '<CheckStatusRequest>';
-        //$xml += '<RequestTimestamp>' + new Date().toISOString() + '</RequestTimestamp>';
-        //$xml += '<RequestorRef>A6F762</RequestorRef>';
-        //$xml += '</CheckStatusRequest>';
-
-
-        // Vehicle monitoring request
-        //$xml += '<ServiceRequest>';
-        //$xml += '<RequestTimestamp>' + new Date().toISOString() + '</RequestTimestamp>';
-        //$xml += '<RequestorRef>A6F762</RequestorRef>';
-        //$xml += '<VehicleMonitoringRequest version="2.0">';
-        //$xml += '<RequestTimestamp>' + new Date().toISOString() + '</RequestTimestamp>';
-        //$xml += '<VehicleMonitoringRef>VM_ACT_0200</VehicleMonitoringRef>';
-        //$xml += '</VehicleMonitoringRequest>';
-        //$xml += '</ServiceRequest>';
-
-
-        // BusStop Monitoring request
-        $xml += '<ServiceRequest>';
-        $xml += '<RequestTimestamp>' + new Date().toISOString() + '</RequestTimestamp>';
-        $xml += '<RequestorRef>A6F762</RequestorRef>';
-        $xml += '<StopMonitoringRequest version="2.0">';
-        $xml += '<PreviewInterval>PT60M</PreviewInterval>';
-        $xml += '<RequestTimestamp>' + new Date().toISOString() + '</RequestTimestamp>';
-        $xml += '<MonitoringRef>' + getQueryVariable('busStopId') + '</MonitoringRef>';
-        $xml += '</StopMonitoringRequest>';
-        $xml += '</ServiceRequest>';
-
-
-        $xml += '</Siri>';
-
-
-        $.ajax({
-            // url: 'https://cors-anywhere.herokuapp.com/http://siri.nxtbus.act.gov.au:11000/A6F762/vm/status.xml',
-            url: 'https://cors-anywhere.herokuapp.com/http://siri.nxtbus.act.gov.au:11000/A6F762/sm/service.xml',
-            data: $xml,
-            type: 'POST',
-            contentType: "text/xml",
-            dataType: "text",
-            success: function (xml) {
-                TweenMax.to('.loader', 0.75, {
-                    autoAlpha: 0,
-                    scale: 0,
-                    ease: Expo.easeOut,
-                    onComplete: function () {
-                        $('.loader').remove();
-                        processData(xml);
-                    }
-                });
-            },
-            error: function (msg) {
-                console.log(error);
-
-                toaster('Whoops! Something went wrong! Error (' + error.status + ' ' + error.statusText + ')');
-            }
-        });
+        lookupBusId(getQueryVariable('busStopId'), null);
     }
 });
+
+function getQueryVariable(variable) {
+    let query = window.location.search.substring(1),
+        vars = query.split("&");
+
+    for (let i = 0; i < vars.length; i++) {
+        let pair = vars[i].split("=");
+
+        if (pair[0] == variable) {
+            return pair[1];
+        }
+    }
+
+    return false;
+};
+
+let lookupBusId = function (id) {
+    let $xml = '';
+
+    $xml = '<?xml version="1.0" encoding="iso-8859-1" standalone="yes"?>';
+    $xml += '<Siri version="2.0" xmlns:ns2="http://www.ifopt.org.uk/acsb" xmlns="http://www.siri.org.uk/siri" xmlns:ns4="http://datex2.eu/schema/2_0RC1/2_0" xmlns:ns3="http://www.ifopt.org.uk/ifopt">';
+
+
+    // Check status
+    //$xml += '<CheckStatusRequest>';
+    //$xml += '<RequestTimestamp>' + new Date().toISOString() + '</RequestTimestamp>';
+    //$xml += '<RequestorRef>A6F762</RequestorRef>';
+    //$xml += '</CheckStatusRequest>';
+
+
+    // Vehicle monitoring request
+    //$xml += '<ServiceRequest>';
+    //$xml += '<RequestTimestamp>' + new Date().toISOString() + '</RequestTimestamp>';
+    //$xml += '<RequestorRef>A6F762</RequestorRef>';
+    //$xml += '<VehicleMonitoringRequest version="2.0">';
+    //$xml += '<RequestTimestamp>' + new Date().toISOString() + '</RequestTimestamp>';
+    //$xml += '<VehicleMonitoringRef>VM_ACT_0200</VehicleMonitoringRef>';
+    //$xml += '</VehicleMonitoringRequest>';
+    //$xml += '</ServiceRequest>';
+
+
+    // BusStop Monitoring request
+    $xml += '<ServiceRequest>';
+    $xml += '<RequestTimestamp>' + new Date().toISOString() + '</RequestTimestamp>';
+    $xml += '<RequestorRef>A6F762</RequestorRef>';
+    $xml += '<StopMonitoringRequest version="2.0">';
+    $xml += '<PreviewInterval>PT60M</PreviewInterval>';
+    $xml += '<RequestTimestamp>' + new Date().toISOString() + '</RequestTimestamp>';
+    $xml += '<MonitoringRef>' + id + '</MonitoringRef>';
+    $xml += '</StopMonitoringRequest>';
+    $xml += '</ServiceRequest>';
+
+
+    $xml += '</Siri>';
+
+
+    $.ajax({
+        // url: 'https://cors-anywhere.herokuapp.com/http://siri.nxtbus.act.gov.au:11000/A6F762/vm/status.xml',
+        url: 'https://cors-anywhere.herokuapp.com/http://siri.nxtbus.act.gov.au:11000/A6F762/sm/service.xml',
+        data: $xml,
+        type: 'POST',
+        contentType: "text/xml",
+        dataType: "text",
+        success: function (xml) {
+            TweenMax.to('.loader', 0.75, {
+                autoAlpha: 0,
+                scale: 0,
+                ease: Expo.easeOut,
+                onComplete: function () {
+                    $('.loader').remove();
+                    processData(xml);
+                }
+            });
+        },
+        error: function (msg) {
+            console.log(error);
+
+            toaster('Whoops! Something went wrong! Error (' + error.status + ' ' + error.statusText + ')');
+        }
+    });
+};
 
 function processData(xml) {
     let xmlDoc = $.parseXML(xml),
@@ -88,13 +107,16 @@ function processData(xml) {
         serviceNum = '',
         arr = '',
         eta = '',
-        etaMin = '';
+        etaMin = '',
+        icon = '';
 
-    obj = {
-        busStopName: decodeURIComponent(getQueryVariable('busStopName'))
-    };
+    if (getQueryVariable('busStopName')) {
+        obj = {
+            busStopName: decodeURIComponent(getQueryVariable('busStopName'))
+        };
 
-    cardMarkup += cardHeader(obj);
+        cardMarkup += cardHeader(obj);
+    }
 
     if ($monitoredStopVisit.length) {
         for (let i = 0, l = $monitoredStopVisit.length; i < l; i++) {
@@ -104,13 +126,16 @@ function processData(xml) {
                 arr = new Date($($monitoredStopVisit[i]).find('ExpectedArrivalTime')[0].innerHTML);
             }
 
+            vehicleFeatureArr = [];
             vehicleFeatureRef = $($monitoredStopVisit[i]).find('VehicleFeatureRef'),
             eta = arr.getTime() - now.getTime(); // This will give difference in milliseconds
             etaMin = Math.round(eta / 60000);
             serviceNum = $($monitoredStopVisit[i]).find('PublishedLineName')[0].innerHTML;
 
             for (let j = 0, m = vehicleFeatureRef.length; j < m; j++) {
-                vehicleFeatureArr.push(vehicleFeatureRef[j].innerHTML);
+                icon = vehicleFeatureRef[j].innerHTML;
+                icon = icon.replace(' ', '-').toLowerCase();
+                vehicleFeatureArr.push(icon);
             }
 
             obj = {
@@ -125,7 +150,7 @@ function processData(xml) {
         // cardMarkup += cardEmptyTemplate({});
     }
 
-    $('.timetable .col-12').html(cardMarkup);
+    $('.cards-wrapper.col-12').html(cardMarkup);
 
     TweenMax.staggerTo('.card', 0.75, {
         opacity: 1,
@@ -134,41 +159,4 @@ function processData(xml) {
     }, 0.1);
 };
 
-function updateEta(el, json) {
-    let services = json.Services,
-        now = new Date(),
-        arr,
-        eta,
-        etaMin;
-
-    arr = new Date(services[0].NextBus.EstimatedArrival);
-    eta = arr.getTime() - now.getTime(); // This will give difference in milliseconds
-    etaMin = Math.round(eta / 60000);
-
-    if (etaMin < 0) {
-        el.find('.eta').html('<p class="departed">Departed</p>');
-    } else if (etaMin == 0) {
-        el.find('.eta').html('<p class="arriving">Arriving</p>');
-    } else if (etaMin == 1) {
-        el.find('.eta').html('<p class="near">' + etaMin + ' min</p>');
-    } else {
-        el.find('.eta').html('<p>' + etaMin + ' mins</p>');
-    }
-
-    el.find('.eta').text();
-};
-
-function getQueryVariable(variable) {
-    let query = window.location.search.substring(1),
-        vars = query.split("&");
-
-    for (let i = 0; i < vars.length; i++) {
-        let pair = vars[i].split("=");
-
-        if (pair[0] == variable) {
-            return pair[1];
-        }
-    }
-
-    return (false);
-};
+export { lookupBusId }
