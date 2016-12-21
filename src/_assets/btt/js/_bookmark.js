47341609323b3f2bdba6273501211e746af9038e
Nino Ross Rodriguez <oninross@gmail.com>
2016-12-22 07:28:35 +1100

    - Displaying bus stop name

--- src/_assets/btt/js/_bookmark.js
+++ src/_assets/btt/js/_bookmark.js
@@ -6,8 +6,10 @@ import { lookupBusId } from './_busStop';
 
 let loader = '<div class="loader"><svg class="circular" viewBox="25 25 50 50"><circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="4" stroke-miterlimit="10"/></svg></div>',
     obj = {},
+    services = {},
     tmpArr = [],
     tmpArrInd = 0,
+    busStopName = '',
     strArr = '';
 
 $(() => {
@@ -15,23 +17,50 @@ $(() => {
         cardMarkup = '';
 
     if ($('.bookmark').length) {
-        tmpArr = JSON.parse(localStorage.bookmarks);
+        $.ajax({
+            url: '/assets/btt/api/services.json',
+            success: function (data) {
+                services = data;
 
-        for (let i = 0, l = tmpArr.length; i < l; i++) {
-            obj = {
-                busStopId: tmpArr[i]
-            };
+                tmpArr = JSON.parse(localStorage.bookmarks);
 
-            cardMarkup += cardBookmark(obj);
-        }
+                for (let i = 0, l = tmpArr.length; i < l; i++) {
+                    // iterate over each element in the array
+                    for (let j = 0, m = services.length; j < m; j++){
+                        // look for the entry with a matching `code` value
+                        if (services[j].data == tmpArr[i]){
+                            // we found it
+                            // obj[i].name is the matched result
+                            busStopName = services[j].name;
+                            console.log(busStopName)
+                        }
+                    }
 
-        $('.cards-wrapper.col-12').html(cardMarkup);
+                    obj = {
+                        busStopId: tmpArr[i],
+                        busStopName: busStopName
+                    };
 
-        TweenMax.staggerTo('.card', 0.75, {
-            opacity: 1,
-            top: 1,
-            ease: Expo.easeOut
-        }, 0.1);
+                    cardMarkup += cardBookmark(obj);
+                }
+
+                $('.cards-wrapper.col-12').html(cardMarkup);
+
+                TweenMax.staggerTo('.card', 0.75, {
+                    opacity: 1,
+                    top: 1,
+                    ease: Expo.easeOut
+                }, 0.1);
+            },
+            error: function (error) {
+                console.log(error);
+
+                toaster('Whoops! Something went wrong! Error (' + error.status + ' ' + error.statusText + ')');
+            },
+            statusCode: function (code) {
+                console.log(code);
+            }
+        });
     }
 });
 
