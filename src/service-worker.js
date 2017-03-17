45e0f054737e49c18d6d286e57bf7b4f7320dccc
Nino Ross Rodriguez <oninross@gmail.com>
2017-03-17 15:25:01 +1100

    - version number bump

--- src/service-worker.js
+++ src/service-worker.js
@@ -28,7 +28,7 @@ importScripts('serviceworker-cache-polyfill.js');
 // cache, then increment the CACHE_VERSION value. It will kick off the service worker update
 // flow and the old cache(s) will be purged as part of the activate event handler when the
 // updated service worker is activated.
-var version = '0.12.0',
+var version = '0.12.1',
     OFFLINE_URL = 'offline/',
     CURRENT_CACHES = {
         prefetch: 'prefetch-cache-v' + version
