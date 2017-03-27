b538847463eeacd5650ee605838fad3f7f3db7be
Nino Ross Rodriguez <oninross@gmail.com>
2017-03-27 21:35:15 +1100

    - minor js fix on checking if notification is permitted

--- src/_assets/btt/js/_trackmybus.js
+++ src/_assets/btt/js/_trackmybus.js
@@ -169,7 +169,7 @@ export default class TrackMyBus {
 
             if (isServiceWorkerSupported()) {
                 google.maps.event.addListener(stopMarker, 'click', function (e) {
-                    if (isNotificationGranted) {
+                    if (isNotificationGranted()) {
                         let busStopId = $(this)[0].id;
                         that.notifyMe(busStopId);
 
