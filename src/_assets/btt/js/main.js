fdfcd91121c578e9cdd3cd3f038a25798156a912
Nino Ross Rodriguez <oninross@gmail.com>
2017-05-05 18:37:22 +1000

    - version number bump

--- src/_assets/btt/js/main.js
+++ src/_assets/btt/js/main.js
@@ -100,105 +100,105 @@ function urlBase64ToUint8Array(base64String) {
     return outputArray;
 }
 
-// if (isServiceWorkerSupported()) {
-//     window.addEventListener('load', function () {
-//         navigator.serviceWorker.register('/service-worker.js')
-//         .then(function (registration) {
-//             registration.onupdatefound = function () {
-//                 // The updatefound event implies that registration.installing is set; see
-//                 // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
-//                 const installingWorker = registration.installing;
+if (isServiceWorkerSupported()) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/service-worker.js')
+        .then(function (registration) {
+            registration.onupdatefound = function () {
+                // The updatefound event implies that registration.installing is set; see
+                // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
+                const installingWorker = registration.installing;
 
-//                 installingWorker.onstatechange = function () {
-//                     switch (installingWorker.state) {
-//                         case 'installed':
-//                             if (!navigator.serviceWorker.controller) {
-//                                 toaster('Caching complete!');
-//                             }
-//                             break;
+                installingWorker.onstatechange = function () {
+                    switch (installingWorker.state) {
+                        case 'installed':
+                            if (!navigator.serviceWorker.controller) {
+                                toaster('Caching complete!');
+                            }
+                            break;
 
-//                         case 'redundant':
-//                             throw Error('The installing service worker became redundant.');
-//                     }
-//                 };
-//             };
+                        case 'redundant':
+                            throw Error('The installing service worker became redundant.');
+                    }
+                };
+            };
 
 
-//             // Use the PushManager to get the user's subscription to the push service.
+            // Use the PushManager to get the user's subscription to the push service.
 
-//             //service worker.ready will return the promise once the service worker is registered. This can help to get rid of
-//             //errors that occur while fetching subscription information before registration of the service worker
+            //service worker.ready will return the promise once the service worker is registered. This can help to get rid of
+            //errors that occur while fetching subscription information before registration of the service worker
 
-//             return navigator.serviceWorker.ready.then(function (serviceWorkerRegistration) {
-//                 return serviceWorkerRegistration.pushManager.getSubscription()
-//                     .then(function (subscription) {
+            return navigator.serviceWorker.ready.then(function (serviceWorkerRegistration) {
+                return serviceWorkerRegistration.pushManager.getSubscription()
+                    .then(function (subscription) {
 
-//                         // If a subscription was found, return it.
-//                         if (subscription) {
-//                             return subscription;
-//                         }
+                        // If a subscription was found, return it.
+                        if (subscription) {
+                            return subscription;
+                        }
 
-//                         const convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey);
+                        const convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey);
 
-//                         // Otherwise, subscribe the user (userVisibleOnly allows to specify that we don't plan to
-//                         // send browser push notifications that don't have a visible effect for the user).
-//                         return serviceWorkerRegistration.pushManager.subscribe({
-//                             userVisibleOnly: true,
-//                             applicationServerKey: convertedVapidKey
-//                         });
-//                     });
-//             });
-//         }).then(function (subscription) { //chaining the subscription promise object
+                        // Otherwise, subscribe the user (userVisibleOnly allows to specify that we don't plan to
+                        // send browser push notifications that don't have a visible effect for the user).
+                        return serviceWorkerRegistration.pushManager.subscribe({
+                            userVisibleOnly: true,
+                            applicationServerKey: convertedVapidKey
+                        });
+                    });
+            });
+        }).then(function (subscription) { //chaining the subscription promise object
 
-//             // Retrieve the user's public key.
-//             var rawKey = subscription.getKey ? subscription.getKey('p256dh') : '';
-//             key = rawKey ? btoa(String.fromCharCode.apply(null, new Uint8Array(rawKey))) : '';
-//             var rawAuthSecret = subscription.getKey ? subscription.getKey('auth') : '';
-//             authSecret = rawAuthSecret ? btoa(String.fromCharCode.apply(null, new Uint8Array(rawAuthSecret))) : '';
+            // Retrieve the user's public key.
+            var rawKey = subscription.getKey ? subscription.getKey('p256dh') : '';
+            key = rawKey ? btoa(String.fromCharCode.apply(null, new Uint8Array(rawKey))) : '';
+            var rawAuthSecret = subscription.getKey ? subscription.getKey('auth') : '';
+            authSecret = rawAuthSecret ? btoa(String.fromCharCode.apply(null, new Uint8Array(rawAuthSecret))) : '';
 
-//             endpoint = subscription.endpoint;
+            endpoint = subscription.endpoint;
 
-//             // Send the subscription details to the server using the Fetch API.
-//             window.II.pushData = {
-//                 endpoint: endpoint,
-//                 key: key,
-//                 authSecret: authSecret
-//             };
-//         })
-//         .catch(function (whut) {
-//             console.error('uh oh... ');
-//             console.error(whut);
-//             toaster('Service worker registration failed:', e);
-//         });
-//     });
+            // Send the subscription details to the server using the Fetch API.
+            window.II.pushData = {
+                endpoint: endpoint,
+                key: key,
+                authSecret: authSecret
+            };
+        })
+        .catch(function (whut) {
+            console.error('uh oh... ');
+            console.error(whut);
+            toaster('Service worker registration failed:', e);
+        });
+    });
 
-//     window.addEventListener('beforeinstallprompt', function (e) {
-//         ga('send', 'event', 'Prompt to add to homescreen', 'click');
+    window.addEventListener('beforeinstallprompt', function (e) {
+        ga('send', 'event', 'Prompt to add to homescreen', 'click');
 
-//         // e.userChoice will return a Promise. For more details read: http://www.html5rocks.com/en/tutorials/es6/promises/
-//         e.userChoice.then(function (choiceResult) {
-//             console.log(choiceResult.outcome);
+        // e.userChoice will return a Promise. For more details read: http://www.html5rocks.com/en/tutorials/es6/promises/
+        e.userChoice.then(function (choiceResult) {
+            console.log(choiceResult.outcome);
 
-//             if (choiceResult.outcome == 'dismissed') {
-//                 console.log('User cancelled homescreen install');
-//                 ga('send', 'event', 'Cancelled add to homescreen', 'click');
-//             } else {
-//                 console.log('User added to homescreen');
-//                 ga('send', 'event', 'Added to homescreen', 'click');
-//             }
-//         });
-//     });
+            if (choiceResult.outcome == 'dismissed') {
+                console.log('User cancelled homescreen install');
+                ga('send', 'event', 'Cancelled add to homescreen', 'click');
+            } else {
+                console.log('User added to homescreen');
+                ga('send', 'event', 'Added to homescreen', 'click');
+            }
+        });
+    });
 
-//     // Check to see if the service worker controlling the page at initial load
-//     // has become redundant, since this implies there's a new service worker with fresh content.
-//     if (navigator.serviceWorker && navigator.serviceWorker.controller) {
-//         console.log("navigator.serviceWorker.controller.onstatechange:: " + navigator.serviceWorker.controller.onstatechange)
-//         navigator.serviceWorker.controller.onstatechange = function (event) {
-//             if (event.target.state === 'redundant') {
-//                 // toaster('A new version of this app is available. Please reload the page.'); // duration 0 indications shows the toast indefinitely.
-//                 window.location.reload();
-//             }
-//         };
-//     }
-// }
+    // Check to see if the service worker controlling the page at initial load
+    // has become redundant, since this implies there's a new service worker with fresh content.
+    if (navigator.serviceWorker && navigator.serviceWorker.controller) {
+        console.log("navigator.serviceWorker.controller.onstatechange:: " + navigator.serviceWorker.controller.onstatechange)
+        navigator.serviceWorker.controller.onstatechange = function (event) {
+            if (event.target.state === 'redundant') {
+                // toaster('A new version of this app is available. Please reload the page.'); // duration 0 indications shows the toast indefinitely.
+                window.location.reload();
+            }
+        };
+    }
+}
 
