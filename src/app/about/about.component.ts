6485f1e08620a17d454f5befb08741377ed36a0c
Nino Ross Rodriguez <ninoross.rodriguez@adelphi.digital>
2018-02-02 15:24:28 +1100

    - code cleanup.  putting code in the right places

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { TweenMax, Expo } from 'gsap/src/uncompressed/TweenMax';
-import 'gsap/src/uncompressed/plugins/ScrollToPlugin';
-
 
 @Component({
     selector: 'app-about',
@@ -16,45 +13,6 @@ export class AboutComponent implements OnInit {
 
     constructor() { }
 
-    ngOnInit() {
-        const accordion = document.getElementsByClassName('accordion'),
-            accordionHeader = document.getElementsByClassName('accordion__header'),
-            accordionBtn = document.getElementsByClassName('accordion__btn'),
-            event = document.createEvent('HTMLEvents');
-
-        event.initEvent('click', false, true);
-
-        for (let i = 0, l = accordion.length; i < l; i++) {
-            // accordion header EventListeners
-            accordionHeader[i].addEventListener('click', function (e) {
-                e.preventDefault();
-
-                this.querySelector('.accordion__btn').dispatchEvent(event);
-            });
-
-            // accortion button EventListeners
-            accordionBtn[i].addEventListener('click', function (e) {
-                e.preventDefault();
-                e.stopPropagation();
-
-                let accordion = this.parentNode.parentNode;
-
-                // Vanilla JS of $(el).toggleClass();
-                if (accordion.classList.value.indexOf('active') > -1) {
-                    accordion.classList.remove('active');
-                } else {
-                    accordion.classList.add('active');
-                }
-
-                console.log(accordion)
-
-                TweenMax.to(window, 1, {
-                    scrollTo: parseInt(accordion.offsetTop),
-                    ease: Expo.easeOut,
-                    delay: 0.5
-                });
-            });
-        }
-    }
+    ngOnInit() { }
 
 }
