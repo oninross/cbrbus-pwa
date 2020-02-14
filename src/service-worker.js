f00b1a846c8d6390b67870135838684c1c817913
Nino Ross Rodriguez <ninoross.rodriguez@adelphi.digital>
2020-02-14 16:28:05 +1100

    - enabled service workers - version number bump

--- src/service-worker.js
+++ src/service-worker.js
@@ -24,7 +24,7 @@
 // cache, then increment the CACHE_VERSION value. It will kick off the service worker update
 // flow and the old cache(s) will be purged as part of the activate event handler when the
 // updated service worker is activated.
-var version = '0.17.9',
+var version = '0.17.10',
     now = Date.now(),
     OFFLINE_URL = 'offline/',
     urlsToPrefetch = [
